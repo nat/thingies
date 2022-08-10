88570c86fd712c2a87a4c362e3b871ab81b91f8f
Nat Friedman <nat@nat.org>
2022-08-09 19:25:12 -0700

    Fix bug

--- notrack.py
+++ notrack.py
@@ -31,20 +31,21 @@ pb = pasteboard.Pasteboard()
 
 while True:
     str = pb.get_contents()
-    orig = str
-    if str is not None and str.startswith('https://twitter.com/') and str.find('t=') != -1:
-        str = str.split('?')[0]
-        pb.set_contents(str)
+    if str is not None:
+        orig = str
+        if str.startswith('https://twitter.com/') and str.find('t=') != -1:
+            str = str.split('?')[0]
+            pb.set_contents(str)
 
-    if str.startswith('http') and (str.find('utm_') != -1 or str.find('st=') != -1 or str.find('reflink') != -1):
-        params = str.split('?')[1]
-        params = params.split('&')
-        params = [x for x in params if not x.startswith('utm_') and not x.startswith('reflink') and not x.startswith('st=')]
-        params = '&'.join(params)
-        str = str.split('?')[0] + ('?' + params if len(params) > 0 else '')
-        pb.set_contents(str)
-    
-    if str != orig:
-        print(string_diff(orig, str))
+        if str.startswith('http') and (str.find('utm_') != -1 or str.find('st=') != -1 or str.find('reflink') != -1):
+            params = str.split('?')[1]
+            params = params.split('&')
+            params = [x for x in params if not x.startswith('utm_') and not x.startswith('reflink') and not x.startswith('st=')]
+            params = '&'.join(params)
+            str = str.split('?')[0] + ('?' + params if len(params) > 0 else '')
+            pb.set_contents(str)
+        
+        if str != orig:
+            print(string_diff(orig, str))
 
     time.sleep (0.5)
