58b755cc19c7c2c46f1c44f8e44ab062dc5f181e
Nat Friedman <nat@nat.org>
2022-08-24 10:56:09 -0700

    Remove ref_src params from Twitter URLs

--- notrack.py
+++ notrack.py
@@ -36,10 +36,11 @@ while True:
         if str.startswith('https://twitter.com/') and str.find('t=') != -1:
             str = str.split('?')[0]
 
-        if str.startswith('http') and (str.find('utm_') != -1 or str.find('st=') != -1 or str.find('reflink') != -1):
+        if str.startswith('http') and (str.find('utm_') != -1 or str.find('st=') != -1 or str.find('reflink') != -1 or str.find('ref_') != -1):
             params = str.split('?')[1]
             params = params.split('&')
-            params = [x for x in params if not x.startswith('utm_') and not x.startswith('reflink') and not x.startswith('st=')]
+            params = [x for x in params if not x.startswith('utm_') and not x.startswith('reflink') and not x.startswith('st=')
+                      and not x.startswith('ref_')]
             params = '&'.join(params)
             str = str.split('?')[0] + ('?' + params if len(params) > 0 else '')
 
